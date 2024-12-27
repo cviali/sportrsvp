9d60f5dbe4d2eed69897ecd9cd9ce47964bff40f
infiniteazuree <christianviali@yahoo.com>
2024-12-28 00:12:56 +0700

    enhance sidebar

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,5 +1,5 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4 d-flex flex-column">
     <!-- Brand Logo -->
     <a href="{{ route('homepage') }}" class="brand-link">
         <img src="{{ asset('bower_components/admin-lte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
@@ -8,18 +8,8 @@
     </a>
 
     <!-- Sidebar -->
-    <div class="sidebar">
+    <div class="sidebar flex-grow-1 d-flex flex-column justify-content-between pb-3">
         <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <div class="img-circle elevation-2 d-flex justify-content-center align-items-center" style="width: 33px; height: 33px;">
-                    <i class="nav-icon fas fa-user-alt text-white"></i>
-                </div>
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">{{ Auth::user()->name }}</a>
-            </div>
-        </div>
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
@@ -48,20 +38,36 @@
                         </p>
                     </a>
                 </li>
-                <li class="nav-item align-item-end">
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                        class="nav-link text-danger">
-                        <i class="nav-icon fas fa-sign-out-alt"></i>
-                        <p>
-                            Logout
-                        </p>
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </li>
             </ul>
         </nav>
+        <div>
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <div class="img-circle elevation-2 d-flex justify-content-center align-items-center" style="width: 33px; height: 33px;">
+                        <i class="nav-icon fas fa-user-alt text-white"></i>
+                    </div>
+                </div>
+                <div class="info">
+                    <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                </div>
+            </div>
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <li class="nav-item align-item-end">
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                            class="nav-link text-danger">
+                            <i class="nav-icon fas fa-sign-out-alt"></i>
+                            <p>
+                                Logout
+                            </p>
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
+            </nav>
+        </div>
         <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
